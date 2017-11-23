4a42c9ce5be2cf0abe5ac506f511059b38b5b11d
Anthony Gore <anthony@magnum.technology>
2017-11-23 16:01:48 +1100

    Using heredoc text

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -14,7 +14,13 @@ class AppController extends Controller
 
         ob_start();
 
-        $v8->executeString('var process = { env: { VUE_ENV: "server", NODE_ENV: "production" }}; this.global = { process: process };');
+        $js =
+<<<EOT
+var process = { env: { VUE_ENV: "server", NODE_ENV: "production" } }; 
+this.global = { process: process };
+EOT;
+
+        $v8->executeString($js);
         $v8->executeString($renderer_source);
         $v8->executeString($app_source);
 
